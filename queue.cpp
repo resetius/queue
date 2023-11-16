aefb48cb14f5424f844643332ff652f14c69c3f9
Alexey Ozeritskiy <aozeritsky@gmail.com>
2023-11-17 00:31:12 +0300

    Add mmap

--- queue.cpp
+++ queue.cpp
@@ -1,21 +1,71 @@
 #include <algorithm>
 #include <string.h>
 #include <assert.h>
+#include <atomic>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+struct QueueHeader {
+    std::atomic_int size;
+    std::atomic_int free_size;
+    char mem[0];
+};
+
+class QueueFile {
+public:
+    QueueFile(const char* file, int capacity, bool attach)
+        : fd(open_file(file, capacity, attach))
+        , capacity(capacity)
+        , data((QueueHeader*)mmap(nullptr, capacity + sizeof(QueueHeader), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0))
+    {
+        if (!attach) {
+            data->size = 0;
+            data->free_size = capacity;
+        }
+    }
+
+    ~QueueFile()
+    {
+        munmap(data, capacity + sizeof(QueueHeader));
+        close(fd);
+    }
+
+    QueueHeader* header() {
+        return data;
+    }
+
+private:
+    int open_file(const char* file, int capacity, bool attach) {
+        int fd = open(file, O_RDWR | O_CREAT, 0666);
+        if (!attach) {
+            lseek(fd, capacity + sizeof(QueueHeader), SEEK_SET);
+            write(fd, "", 1);
+            lseek(fd, 0, SEEK_SET);
+        }
+        return fd;
+    }
+
+    int fd;
+    int capacity;
+    QueueHeader* data;
+};
 
 class Queue {
 public:
-    Queue(int capacity)
-        : capacity(capacity)
-        , size(0)
-        , free_size(capacity)
-        , mem(new char[capacity])
+    Queue(const char* file, int capacity, bool attach)
+        : qfile(QueueFile(file, capacity, attach))
+        , capacity(capacity)
+        , size(qfile.header()->size)
+        , free_size(qfile.header()->free_size)
+        , mem(qfile.header()->mem)
         , w(0)
         , r(0)
     { }
 
     ~Queue()
     {
-        delete[] mem;
     }
 
     void push(const char* buf, int len) {
@@ -47,17 +97,18 @@ public:
     }
 
 private:
+    QueueFile qfile;
     int capacity;
-    int size;
-    int free_size;
-    int r;
-    int w;
+    std::atomic_int& size;
+    std::atomic_int& free_size;
     char* mem;
+    int w;
+    int r;
 };
 
 int main() {
     char buf[1024];
-    Queue q(1024);
+    Queue q("q", 1024, false);
     q.push("abc", 4);
     q.pop(buf, 4);
 
